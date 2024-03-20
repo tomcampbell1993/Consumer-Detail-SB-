4c1c1edd10da61f212e325db452a29685de1a6ad
Thomas Campbell <thomas.campbell@sainsburysbank.co.uk>
2024-03-20 10:31:16 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>CONSUMER DUTY</h1>
+        <h3>
+          Produced, written and directed by Web Team Supreme (jazz interlude) +
+          1 other
+        </h3>
+      </div>
+    </>
+  );
+}
+
+export default App
